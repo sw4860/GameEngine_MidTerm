576d2bd9c822a436dbf56fd22e89912292f41ea5
sand <68569225+sw4860@users.noreply.github.com>
2026-04-17 19:25:24 +0900

    Ruletile, hp ui animation

--- Assets/Scripts/Manager/UiManager.cs
+++ Assets/Scripts/Manager/UiManager.cs
@@ -1,4 +1,4 @@
-using System;
+using System.Collections;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
@@ -7,14 +7,25 @@ using UnityEngine.SceneManagement;
 public class UiManager : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI HpText;
-    [SerializeField] private Image hpBarImage;
+    [SerializeField] private Image HpMainImage;
+    [SerializeField] private Image HpSubImage;
 
     [SerializeField] private GameObject GameOverPanel;
 
     private Player player;
+    private float hpRatio;
+    [SerializeField] private float hpDecreaseDuration = 0.5f;
+
     void Awake()
     {
-        player = GameObject.FindAnyObjectByType<Player>();
+        player = FindAnyObjectByType<Player>();
+        if (player != null)
+        {
+            hpRatio = player.GetHealth()[0] / player.GetHealth()[1];
+            HpMainImage.fillAmount = hpRatio;
+            HpSubImage.fillAmount = hpRatio;
+            HpText.text = $"{player.GetHealth()[0]}/{player.GetHealth()[1]}";
+        }
         EventManager.OnPlayerHPChanged += OnPlayerHPChanged;
         EventManager.OnPlayerDeath += OnPlayerDeath;
     }
@@ -27,10 +38,44 @@ public class UiManager : MonoBehaviour
 
     void OnPlayerHPChanged()
     {
-        HpText.text = $"{player.GetHealth()[0]}/{player.GetHealth()[1]}";
-        hpBarImage.fillAmount = player.GetHealth()[0] / player.GetHealth()[1];
+        if (player == null) return;
+
+        float currentHealth = player.GetHealth()[0];
+        float maxHealth = player.GetHealth()[1];
+        float currentHealthRatio = currentHealth / maxHealth;
+
+        HpText.text = $"{currentHealth}/{maxHealth}";
+        HpMainImage.fillAmount = currentHealthRatio;
+
+        if (currentHealthRatio < hpRatio)
+        {
+            StopAllCoroutines();
+            StartCoroutine(AnimateHpSubImage(currentHealthRatio));
+        }
+        else if (currentHealthRatio > hpRatio)
+        {
+            HpSubImage.fillAmount = currentHealthRatio;
+        }
+
+        hpRatio = currentHealthRatio;
     }
 
+    IEnumerator AnimateHpSubImage(float targetFillAmount)
+    {
+        float startFillAmount = HpSubImage.fillAmount;
+        float timer = 0f;
+
+        while (timer < hpDecreaseDuration)
+        {
+            timer += Time.deltaTime;
+            float newFillAmount = Mathf.Lerp(startFillAmount, targetFillAmount, timer / hpDecreaseDuration);
+            HpSubImage.fillAmount = newFillAmount;
+            yield return null;
+        }
+
+        HpSubImage.fillAmount = targetFillAmount;
+    }
+    
     void OnPlayerDeath()
     {
         Time.timeScale = 0f;
---
+++ Assets/Scripts/Object/InteractableObject.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class InteractableObject : MonoBehaviour
+{
+    [SerializeField] private string Text;
+    
+}
